659a711284d2e01937e0fe7c468423cb6226c004
Kindziolek <kingapietron@gmail.com>
2022-11-24 21:53:39 +0100

    Task 1

---
+++ main.js
@@ -0,0 +1,19 @@
+// 1. Przypisz zdarzenie mouseenter do wszystkich przekazanych elementów.
+// 2. Po najechaniu na element, odczytaj jego pozycje na stronie.
+// 3. Stwórz dynamicznie element tooltop.
+// 4. Wypozycjonuj element tooltip w odpowiednim miejscu i wstaw go na stronę. 
+// 5. Przypisz zdarzenie mouseleave do wszystkich przekazanych elementów. 
+// 6. Po zjechaniu z elementu, usuń tooltip ze strony. 
+// 7. Zamknij kod w module. 
+
+function showTooltip() {}
+
+
+
+function init(elems) {
+    for(let elem of elems) {
+        elem.addEventListener("mouseenter", showTooltip);
+    }
+}
+
+init(document.querySelectorAll("[title"));
